ba7374c3937f24d4693f9663f92eb202b8ed864e
donco <colin.donald@outlook.com>
2020-01-02 16:24:32 -0800

    Update map_finder.R
    Aesthetic and functionality updates

--- map_finder.R
+++ map_finder.R
@@ -9,6 +9,8 @@ report_areas <- sf::st_read(
   layer = 'Report_Units_HUC08',
   stringsAsFactors = FALSE) 
 
+logo <- base64enc::base64encode("//deqhq1/WQNPS/Status_and_Trend_Reports/Figures/DEQ-logo-color-non-transp71x107.png")
+
 report_areas <- st_transform(report_areas, 4326)
 
 report_name_abr <- list("Black Rock Desert-Humboldt"="blackrock",
@@ -60,26 +62,46 @@ map_name_abr <- list("Black Rock Desert-Humboldt"="blackrock",
                      "Willamette-Santiam-Upper"="will_san_upper",
                      "Willamette-Tualatin"="will_tualatin")
 
+basin_shp <- report_areas %>% group_by(REPORT) %>% summarise(basin = unique(REPORT))
+map_shp <- report_areas %>% group_by(MAP) %>% summarise(basin = unique(MAP),
+                                                        subbasins = paste(unique(HU_8_NAME), collapse = "<br>"))
+
 pal <- colorFactor(palette = "Paired",
-            domain = unique(report_areas$MAP))
+                   domain = unique(map_shp$MAP))
+
+map_shp <- map_shp %>% mutate(map_link = paste0("<a href='", report_name_abr[basin], "/", map_name_abr[MAP], 
+                                                          "_map.html' style='width:600px' target='_blank'>", MAP, "</a>"),
+                                        color = pal(MAP))
 
 report_areas <- report_areas %>% mutate(map_link = paste0("<a href='", report_name_abr[REPORT], "/", map_name_abr[MAP], 
                                                           "_map.html' style='width:600px' target='_blank'>", MAP, "</a>"),
                                         color = pal(MAP))
 
 map_locator <- leaflet(report_areas) %>% addTiles() %>% 
+  addPolygons(data = map_shp, group = "Map Polygons",
+              label = ~lapply(paste0("<b>Map:</b> ", MAP,
+                                     "<br><b>Basin:</b> ", basin,
+                                     "<br><b>Subbasins:</b><br>", subbasins,
+                                     "<br><b>Click area for link to map"), htmltools::HTML),
+              popup = ~paste0("Status and Trends Map: ", map_link), 
+              stroke = FALSE, fill = TRUE, fillOpacity = 0.5, fillColor = ~color,
+              highlightOptions = highlightOptions(fillColor = "black", fillOpacity = 0.75)) %>% 
   addPolygons(group = "Subbasin Polygons",
-              label = ~lapply(paste0("<b>Basin:</b> ", REPORT,
-                                     "<br><b>Subbasin:</b> ", HU_8_NAME,
-                                     "<br><b>Map:</b> ", MAP,
-                                     "<br><b>Click for link to map"), htmltools::HTML),
-              popup = ~paste0("Status and Trends Map: ", map_link),
-              color = "black", weight = 2, opacity = 1, 
-              fillColor = ~color, fillOpacity = 0.6) %>% 
-  addLayersControl(overlayGroups = c("Subbasin Polygons")) %>%
+              label = ~lapply(paste("<b>Subbasin:</b>", HU_8_NAME), htmltools::HTML),
+              color = "black", weight = 2, opacity = 1, fill = FALSE,
+              highlightOptions = highlightOptions(color = "black", weight = 8, opacity = 1)) %>% 
+  addPolygons(data = basin_shp, group = "Basin Polygons",
+              color = "black", weight = 4, opacity = 1, fill = FALSE, 
+              label = ~lapply(paste("<b>Basin:</b>", REPORT), htmltools::HTML),
+              highlightOptions = highlightOptions(color = "black", weight = 8, opacity = 1)) %>% 
+  addLayersControl(overlayGroups = c("Subbasin Polygons", "Basin Polygons", "Map Polygons")) %>%
+  addControl(position = "bottomright", className = "logo",
+             html = sprintf('<html><body><div style="opacity:1">
+                                        <img width="60" src="data:image/png;base64,%s">
+                            </div></body></html>', logo)) %>% 
   addControl(position = "bottomleft", className = "info", 
-             html = "Hover over the map to determine the name of the basin, subbasin, and associated status and trends map.<br> Click on an area of interest to obtain a link to the relevant status and trends map.") %>% 
-  leaflet.extras::addSearchFeatures(targetGroups = "Subbasin Polygons",
+             html = "Hover over the map to determine the name of the basin, subbasin, and relevant status and trends map.<br> Click on an area of interest to obtain a link to its associated status and trends map.") %>% 
+  leaflet.extras::addSearchFeatures(targetGroups = "Map Polygons",
                                     options = searchFeaturesOptions(openPopup = TRUE, textPlaceholder = "Search basins..."))
 
 htmlwidgets::saveWidget(map_locator, paste0("//deqhq1/WQNPS/Status_and_Trend_Reports/2019/web_wqst_2019/2019_map_locator.html"), 
